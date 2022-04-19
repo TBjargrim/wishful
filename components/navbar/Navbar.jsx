f1e2582f5ada0eafe3c1ef9f66602287c3584eae
--unset <therese.bjargrim@student.KYH.se>
2022-04-19 09:52:45 +0200

    Updates on burger-menu button styling

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -14,58 +14,69 @@ const Navbar = () => {
   console.log(openMenu);
   return (
     <>
-      <section className={styles.navbarContainer}>
-        <Link href="/hemsida">
-          <a aria-label="link to homepage">
-            <NextImage src="/logo_2.svg" alt="logo" width="90" height="90" />
-          </a>
-        </Link>
+      <section className={`${styles.navbarContainer} ${burgerMenuClass}`}>
+        {!openMenu && (
+          <>
+            <Link href="/hemsida">
+              <a aria-label="link to homepage">
+                <NextImage
+                  src="/logo_2.svg"
+                  alt="logo"
+                  width="90"
+                  height="90"
+                />
+              </a>
+            </Link>
 
-        <div>
-          <ul className={styles.navbarList}>
-            <li>
-              <Link href="/hemsida">
-                <a aria-label="link to homepage">Hem</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/min-profil">
-                <a aria-label="link to mypage">Min sida</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/vanner">
-                <a aria-label="link to friendspage">Vänner</a>
-              </Link>
-            </li>
-          </ul>
-          <NextImage src="/avatar_1.svg" alt="avatar" width="64" height="64" />
-        </div>
-
-        <div className={`${styles.mobileContainer} ${burgerMenuClass}`}>
-          <label for="check">
-            <input type="checkbox" id="check" onClick={handleClick} />
-            <span></span>
-            <span></span>
-            <span></span>
-          </label>
+            <div>
+              <ul className={styles.navbarList}>
+                <li>
+                  <Link href="/hemsida">
+                    <a aria-label="link to homepage">Hem</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/min-profil">
+                    <a aria-label="link to mypage">Min sida</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/vanner">
+                    <a aria-label="link to friendspage">Vänner</a>
+                  </Link>
+                </li>
+              </ul>
+              <NextImage
+                src="/avatar_1.svg"
+                alt="avatar"
+                width="64"
+                height="64"
+              />
+            </div>
+          </>
+        )}
+        <div
+          onClick={handleClick}
+          className={`${styles.mobileContainer} ${burgerMenuClass}`}
+        >
+          <div className={styles.menuButton}></div>
         </div>
       </section>
 
       {openMenu && (
         <div className={styles.mobileList}>
           <ul>
-            <li>
+            <li onClick={() => setOpenMenu(!openMenu)}>
               <Link href="/hemsida">
                 <a aria-label="link to homepage">Hem</a>
               </Link>
             </li>
-            <li>
+            <li onClick={() => setOpenMenu(!openMenu)}>
               <Link href="/min-profil">
                 <a aria-label="link to mypage">Min sida</a>
               </Link>
             </li>
-            <li>
+            <li onClick={() => setOpenMenu(!openMenu)}>
               <Link href="/vanner">
                 <a aria-label="link to friendspage">Vänner</a>
               </Link>
