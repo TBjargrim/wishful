57e0788344bb891d09a19746593afb79e8e98495
Sandra Persson <sandra.persson@student.kyh.se>
2022-05-04 14:47:04 +0200

    changed styling on calendar

--- pages/hem.js
+++ pages/hem.js
@@ -1,21 +1,17 @@
 import styles from '../styles/_homepage.module.scss';
 import NextImage from 'next/image';
-
 import format from 'date-fns/format';
 import getDay from 'date-fns/getDay';
 import parse from 'date-fns/parse';
+import { sv } from 'date-fns/locale';
 import startOfWeek from 'date-fns/startOfWeek';
-import React, { useState } from 'react';
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const Hem = () => {
-  const [date, setDate] = useState(new Date());
-
   const locales = {
-    'en-US': require('date-fns/locale/en-US'),
+    sv: sv,
   };
 
   const localizer = dateFnsLocalizer({
@@ -26,24 +22,93 @@ const Hem = () => {
     locales,
   });
 
+  const languageChange = {
+    allDay: 'Hela dagen',
+    previous: '<',
+    next: '>',
+    today: 'Idag',
+    month: 'Månad',
+    week: 'Vecka',
+    day: 'Dag',
+    agenda: 'Översikt',
+    date: 'Datum',
+    time: 'Tid',
+    event: 'Event',
+    showMore: (total) => `+ ${total} till`,
+  };
+
   const events = [
     {
       title: 'Sandras födelsedag',
+      type: 'birthday',
+      color: '#6E97FF',
       // allDay: true,
-      start: new Date(2022, 4, 3),
-      end: new Date(2022, 4, 3),
+      start: new Date(2022, 4, 4),
+      end: new Date(2022, 4, 4),
     },
     {
       title: 'Kalles födelsedag',
+      type: 'birthday',
+      color: '#6E97FF',
       // allDay: true,
-      start: new Date(2022, 4, 3),
-      end: new Date(2022, 4, 3),
+      start: new Date(2022, 4, 4),
+      end: new Date(2022, 4, 4),
+    },
+    {
+      title: 'Sandras födelsedag',
+      type: 'birthday',
+      color: '#6E97FF',
+      // allDay: true,
+      start: new Date(2022, 4, 4),
+      end: new Date(2022, 4, 4),
+    },
+    {
+      title: 'Sandras födelsedag',
+      type: 'birthday',
+      color: '#6E97FF',
+      // allDay: true,
+      start: new Date(2022, 4, 4),
+      end: new Date(2022, 4, 4),
+    },
+    {
+      title: 'Pernillas bröllopsdag',
+      type: 'wedding',
+      color: '#BF6EFF',
+      // allDay: true,
+      start: new Date(2022, 4, 5),
+      end: new Date(2022, 4, 5),
+    },
+    {
+      title: 'Lisas fest',
+      type: 'party',
+      color: '#FFC855',
+      // allDay: true,
+      start: new Date(2022, 4, 8),
+      end: new Date(2022, 4, 8),
     },
     {
       title: 'Therese och Bens bröllopsdag',
+      type: 'wedding',
+      color: '#BF6EFF',
       // allDay: true,
       start: new Date(2022, 4, 12),
       end: new Date(2022, 4, 12),
+    },
+    {
+      title: 'Lenas födelsedag',
+      type: 'birthday',
+      color: '#6E97FF',
+      // allDay: true,
+      start: new Date(2022, 4, 13),
+      end: new Date(2022, 4, 13),
+    },
+    {
+      title: 'Lars Bröllopsdag',
+      type: 'wedding',
+      color: '#BF6EFF',
+      // allDay: true,
+      start: new Date(2022, 4, 23),
+      end: new Date(2022, 4, 23),
     },
   ];
 
@@ -154,16 +219,18 @@ const Hem = () => {
               <p>Du har inga påminnelser just nu</p>
             </div> */}
           </div>
-
           <div className={styles.calender}>
-            {/* <h3>Kalender</h3> */}
-            {/* <Calendar onChange={setDate} value={date}/> */}
-
             <Calendar
+              classname={styles.calendarStyle}
               localizer={localizer}
+              culture={'sv'}
+              messages={languageChange}
               events={events}
               startAccessor="start"
               endAccessor="end"
+              eventPropGetter={(events) => ({
+                style: { backgroundColor: events.color, width: '95%' },
+              })}
             />
           </div>
         </div>
